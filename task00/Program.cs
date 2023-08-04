5e623ea4a0d414562ac992e5fe166b86ca2f2bac
Dj Kulagin <yarkulagin@MacBook-Air-Yar.local>
2023-08-04 11:31:05 +0400

    task00,task01,task03,task09

---
+++ task00/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+Console.WriteLine(result);
+
+
+
+
---
+++ task03/Program.cs
@@ -0,0 +1,41 @@
+﻿/*03. # Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+12:38
+3 -> Среда
+5 -> Пятница
+*/
+
+Console.WriteLine("Введите число от 1-7");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number==1)
+{
+    Console.WriteLine("понедельник");
+}
+if(number==2)
+{
+    Console.WriteLine("вторник");
+}
+if(number==3)
+{
+    Console.WriteLine("среда");
+}
+if(number==4)
+{
+    Console.WriteLine("четверг");
+}
+if(number==5)
+{
+    Console.WriteLine("пятница");
+}
+if(number==6)
+{
+    Console.WriteLine("суббота");
+}
+if(number==7)
+{
+    Console.WriteLine("воскресенье");
+}
+else
+{
+    Console.WriteLine("ошибка");
+}
