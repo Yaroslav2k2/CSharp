fc9f1cb4d137a073af06e2309aaf7a59cf575b4f
Dj Kulagin <yarkulagin@MacBook-Air-Yar.local>
2023-08-04 11:47:36 +0400

    task11

---
+++ task11/Program.cs
@@ -0,0 +1,7 @@
+﻿// напишите программу которая выдает рандомное трех значное число и удаляет вторую цифру
+
+int number = new Random().Next(100,1000);
+Console.WriteLine(number);
+int number1 = number / 100;
+int number3 = number % 10;
+Console.WriteLine($"{number1}{number3}");
