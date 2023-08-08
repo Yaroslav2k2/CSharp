f00418656740e490223bf22207e67978ce27ebe6
Dj Kulagin <yarkulagin@MacBook-Air-Yar.local>
2023-08-08 19:09:01 +0400

    task10 complit

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+int number = Convert.ToInt32(Console.ReadLine());
+int numberdvoynoe = number % 100;
+int numbertwo = numberdvoynoe / 10;
+Console.WriteLine($"{numbertwo}");
