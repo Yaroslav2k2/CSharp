3999aa13272eb921ecf52051d998afcf1bdde60b
Dj Kulagin <yarkulagin@MacBook-Air-Yar.local>
2023-08-08 19:41:16 +0400

    task15 complit

--- task15/Program.cs
+++ task15/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+int number = Convert.ToInt32(Console.ReadLine());
+if(number == 6 || number==7)
+{
+    Console.WriteLine("ДА");
+}
+if(number<6)
+{
+    Console.WriteLine("Нет");
+}
+if(number<1 || number>7)
+{
+    Console.WriteLine("Такого дня нет");
+}
