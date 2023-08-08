5f4d6b4ad5a086f93ac0fbd56235c645977bdfc2
Dj Kulagin <yarkulagin@MacBook-Air-Yar.local>
2023-08-08 08:59:03 +0400

    compliet task02

--- task02/Program.cs
+++ task02/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+if(firstnumber > secondnumber)
+{
+    Console.WriteLine($"max = {firstnumber}");
+}
+else
+{
+    Console.WriteLine($"max = {secondnumber}");
+}
