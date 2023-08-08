1de18b6e76b15bd7e36129ca59102e3d4d87efec
Dj Kulagin <yarkulagin@MacBook-Air-Yar.local>
2023-08-08 19:33:19 +0400

    task13 complit

---
+++ task13/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+int number = Convert.ToInt32(Console.ReadLine());
+if(number / 100 > 0)
+{
+    int numbertwo= number %100;
+    Console.WriteLine($"{numbertwo % 10}");
+}
+else
+{
+    Console.WriteLine("Третьего числа нет");
+}
