499fe2de2a584558e9094ef467aacdf2ebaddbf6
Dj Kulagin <yarkulagin@MacBook-Air-Yar.local>
2023-08-08 09:09:49 +0400

    task04 complit

--- task04/Program.cs
+++ task04/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+if(number1 > number2)
+{
+    Console.WriteLine($"{number1}");
+}
+if(number2 > number3)
+{
+    Console.WriteLine($"{number2}");
+}
+else
+{
+    Console.WriteLine($"{number3}");
+}
