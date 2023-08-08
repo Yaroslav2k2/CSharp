f3c7111026354c6eeeb26833a5526a1b16f95567
Dj Kulagin <yarkulagin@MacBook-Air-Yar.local>
2023-08-08 09:33:07 +0400

    task08 complit

--- task08/Program.cs
+++ task08/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+int number = Convert.ToInt32(Console.ReadLine());
+int chetnye=1;
+while(chetnye != number + 1)
+{
+    if(chetnye % 2 == 0)
+    {
+        Console.WriteLine($"{chetnye}");
+    }
+    chetnye = chetnye + 1;
+}
