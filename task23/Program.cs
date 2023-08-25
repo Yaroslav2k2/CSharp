c6cc009212712ad52321b3b610232a8ef867821a
Dj Kulagin <yarkulagin@MacBook-Air-Yar.local>
2023-08-25 13:33:53 +0400

    task23 start

---
+++ task23/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27; 5 -> 1, 8, 27, 64, 125  */
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+for(int i = 0;i > number;i++ )
+{
+    int numbers=(number*number*number);
+    Console.WriteLine($"{numbers}");
+}
+Console.WriteLine($"{numbers}");
