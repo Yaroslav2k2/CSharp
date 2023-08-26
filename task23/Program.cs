fa8b3e676a341e4d07b01b52dda4eef915a1486b
Dj Kulagin <yarkulagin@MacBook-Air-Yar.local>
2023-08-26 14:37:13 +0400

    task23 complit

--- task23/Program.cs
+++ task23/Program.cs
@@ -2,10 +2,10 @@
 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 3 -> 1, 8, 27; 5 -> 1, 8, 27, 64, 125  */
 Console.WriteLine("Введите число");
+int numbers = 0;
 int number = Convert.ToInt32(Console.ReadLine());
-for(int i = 0;i > number;i++ )
+for(int i = 1; i <= number;i++ )
 {
-    int numbers=(number*number*number);
+    numbers=(i*i*i);
     Console.WriteLine($"{numbers}");
 }
-Console.WriteLine($"{numbers}");
