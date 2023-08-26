342774f38ea1bcbd6926beb7d58c9bd6bf47848c
Dj Kulagin <yarkulagin@MacBook-Air-Yar.local>
2023-08-26 14:55:38 +0400

    task24 complit

---
+++ task24/Program.cs
@@ -0,0 +1,12 @@
+﻿/*напишите программу, которая принимает на вход число (А)
+и выдает сумму чисел от 1 до А
+*/
+
+Console.WriteLine("Введите число:");
+int summa=0;
+int numberA = Convert.ToInt32(Console.ReadLine());
+for(int i=1; i<numberA; i++)
+{
+    summa=summa+i;
+}
+Console.WriteLine($"Сумма равна {summa}");
