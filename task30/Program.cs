4e080f25da006767d20258abdc878239f23c9f4a
Dj Kulagin <yarkulagin@MacBook-Air-Yar.local>
2023-08-29 14:59:19 +0400

    task30 complite

---
+++ task30/Program.cs
@@ -0,0 +1,23 @@
+﻿/*напишите программу которая выводит массив из 8 эллементов заполненный нулями и еденицами в случайном порядке*/
+int[] GetRandomArray()
+{
+    int[] array = new int[8];
+    for(int i=0;i < array.Length;i++)
+    {
+        array[i] = new Random().Next(0,2);
+
+    }
+    
+    return array;
+}
+int[] userArray= GetRandomArray();
+void printarray(int[] arraytoprint)
+{
+    System.Console.Write("[");
+    for(int i=0;i < arraytoprint.Length;i++)
+    {
+        System.Console.Write($"{arraytoprint[i]}");
+    }
+    Console.WriteLine("]");
+}
+printarray(userArray);
