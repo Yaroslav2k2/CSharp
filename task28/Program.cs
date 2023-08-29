dbbcb582aa9c0ba601d13bba626d66f3f26a3f98
Dj Kulagin <yarkulagin@MacBook-Air-Yar.local>
2023-08-29 14:25:44 +0400

    task28 complite

---
+++ task28/Program.cs
@@ -0,0 +1,14 @@
+﻿/* напишите программу которая принимает на вход число n и выдает произведение чисел от 1 до n
+*/
+Console.WriteLine("Введите число");
+int usernumber = Convert.ToInt32(Console.ReadLine());
+int Proizvedenie(int number)
+{
+    int proizvedenie=1;
+    for(int i=1;i<=usernumber;i+=1)
+{
+    proizvedenie*=i;
+}
+    return proizvedenie;
+}
+Console.WriteLine($"произведение цифр равно {Proizvedenie(usernumber)}");
