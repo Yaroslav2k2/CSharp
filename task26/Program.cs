370cea4401d7ac4c68d88db1c9fd9473d5f8a589
Dj Kulagin <yarkulagin@MacBook-Air-Yar.local>
2023-08-29 14:14:03 +0400

    task26 complite

---
+++ task26/Program.cs
@@ -0,0 +1,13 @@
+﻿/* yапишите программу которая на входе берет число и выдает кол-во цифр в числе*/
+Console.WriteLine("Введите число");
+int usernumber = Convert.ToInt32(Console.ReadLine());
+int CountOFNumbers(int number)
+{
+    int count=0;
+    for(int i=usernumber;i>0;i/=10)
+{
+    count+=1;
+}
+    return count;
+}
+Console.WriteLine($"количество цифр в числе {usernumber} равно {CountOFNumbers(usernumber)}");
