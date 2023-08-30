9fee7036f94009bef6c26bc15d269919afcac02a
Dj Kulagin <yarkulagin@MacBook-Air-Yar.local>
2023-08-30 09:44:02 +0400

    task27 complite

---
+++ task27/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+Console.WriteLine("Введите число");
+int NumberUser = Convert.ToInt32(Console.ReadLine());
+int DefSummaOfNumbers(int number)
+{
+    int summa = 0;
+    int numbers = 0;
+    for(int i=NumberUser;i>0;i/=10)
+    {
+        numbers = i%10;
+        summa+= numbers;
+    }   
+    return summa;
+}
+Console.WriteLine($"Сумма цифр числа {NumberUser} равна {DefSummaOfNumbers(NumberUser)}");
