9350814537cdf8285216b720cfbb90783126223b
Dj Kulagin <yarkulagin@MacBook-Air-Yar.local>
2023-09-03 10:00:23 +0400

    task32 complite

---
+++ task32/Program.cs
@@ -0,0 +1,37 @@
+﻿/* напишите на вход программу котоая принмает массив и меняет положительные значения на отрицательные*/
+Console.Write("введите числа: ");
+int Size= Convert.ToInt32(Console.ReadLine());
+int Start= Convert.ToInt32(Console.ReadLine());
+int End= Convert.ToInt32(Console.ReadLine());
+int[] Random(int Size, int Start, int End)
+{
+    int[] array = new int[Size];
+    for(int i=0;i<array.Length;i++)
+    {
+        array[i] = new Random().Next(Start,End);
+    }
+    return array;
+}   
+int[] userArray = Random(Size,Start,End);
+void printArray(int[] arraytoprint)
+{
+    Console.Write("[");
+    for(int i=0;i<arraytoprint.Length;i++)
+    {
+        Console.Write($"{arraytoprint[i]}, ");
+    }
+    Console.Write("]; ");
+}
+printArray(userArray);
+Console.Write("Обратный массив:");
+void reversprintArray(int[] reversarraytoprint)
+{
+    Console.Write("[");
+    for(int i=0;i<reversarraytoprint.Length;i++)
+    {
+        reversarraytoprint[i]*=-1;
+        Console.Write($"{reversarraytoprint[i]} ");
+    }
+    Console.Write("]");
+}
+reversprintArray(userArray);
