a9dddf4d4e1fa6f957a1ee8939c66a8101d026c9
Dj Kulagin <yarkulagin@MacBook-Air-Yar.local>
2023-09-04 19:25:26 +0400

    task43 complite

---
+++ task43/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+ y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+Console.WriteLine("");
+Console.Write("введите числo b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("введите числo k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("введите числo b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("введите числo k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+double x = -(b1 + b2)/(k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
