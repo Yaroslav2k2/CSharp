c8c9026c20cfa8b95ef80251b5fec5b96b3a78e2
Dj Kulagin <yarkulagin@MacBook-Air-Yar.local>
2023-09-15 21:25:56 +0400

    task68 complite

--- task68/Program.cs
+++ task68/Program.cs
@@ -1,18 +1,30 @@
-﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
-M = 1; N = 15 -> 120
-M = 4; N = 8. -> 30
-*/
-Console.Write("Введите M: ");
-int M =Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите N: ");
-int N =Convert.ToInt32(Console.ReadLine());
-void Summa(int M,int N)
+﻿/*
+ Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+ m = 2, n = 3 -> A(m,n) = 9
+ m = 3, n = 2 -> A(m,n) = 29
+ */
+ 
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int Akkerman(int m, int n)
 {
-    int summa=0;
-    for(int i=M;i<=N;i++)
+    if (m == 0)
     {
-        summa+=i;
+        return n + 1;
     }
-    Console.WriteLine($"Сумма = {summa}");
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
 }
-Summa(M,N);
+void AkkermanFunction(int m, int n)
+{
+    Console.WriteLine($"A(m,n)={Akkerman(m, n)}"); 
+}
+AkkermanFunction(m,n);
